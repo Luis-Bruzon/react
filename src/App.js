b8e8a0a31124d35363ba24875bf1352ef6657d9a
Luis Bruzon <bruzon@gmail.com>
2019-10-24 23:36:05 -0300

    epic 4v31

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './MYtineraryLogo.png';
+import logo2 from './circled-right-2.png';
+import logo3 from './homeIcon.png';
 import './App.css';
 
 function App() {
@@ -8,19 +10,24 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Find your perfect trip. designed by insiders who knows and love their cities.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Prueba
-        </a>
-      </header>
+        <p>
+          Start Browsing
+        </p>
+          <img src={logo2} className="App-logo2" alt="logo2" />
+          <p>
+          Want to build your own Mytinerary?
+        </p>
+        <p class="inline-p">Log in </p>
+        <p class="inline-p">Create account </p>
+        <img src={logo3} className="App-logo3" alt="logo3" /> 
+        </header>
     </div>
+    
   );
 }
 
+
 export default App;
+
